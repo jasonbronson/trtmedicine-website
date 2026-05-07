42c3f526ebee39302f1ce04da02d3bb707aaa2b7
Jay <jay@pokergo.com>
2026-05-07 10:38:38 -0400

    1st ver

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#4f55ed',
+        mint: '#26b862',
+        aqua: '#26c2ab',
+        amber: '#f59a33',
+        ink: '#111318'
+      },
+      boxShadow: {
+        phone: '0 32px 90px rgba(17, 19, 24, 0.18)',
+        soft: '0 20px 60px rgba(79, 85, 237, 0.16)'
+      }
+    }
+  },
+  plugins: []
+};
